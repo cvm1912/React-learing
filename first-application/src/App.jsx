1a5ca4ef1cb418e0893544b3bc7dc5c9e9b01590
cvm1912 <rajshivam1912@gmail.com>
2026-03-21 02:13:29 +0530

    feature/components

---
+++ first-application/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Dogcard from './Components/Dogcard'
+
+const App = () => {
+  return (
+    <div>
+      <Dogcard/>
+       <button id='btn'>
+          <span>
+             Submit 
+          </span>
+       </button>
+    </div>
+  )
+}
+
+export default App
---
+++ first-application/src/Components/Dogcard.jsx
@@ -0,0 +1,11 @@
+
+const Dogcard = () => {
+  return (
+    <div>
+      <h3>Bruno</h3>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcPczF9OkeSbIdvP0AiM0Zs_aHZbQaXEs2iRGKuZjq15z3W6EN5wNuq7giecR1AMkP6ytmh1cW5ePbMSSdtM7lcng0MBFIVZaD0_cqVg&s=10" alt="dog image" />
+    </div>
+  )
+}
+
+export default Dogcard
